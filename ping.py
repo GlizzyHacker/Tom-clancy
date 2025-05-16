865547c511c0bc9ccebb07a28af2ffc02837e2d2
Attila Sax <saxattila@gmail.com>
2025-05-16 14:19:40 +0200

    increased ping interval

--- ping.py
+++ ping.py
@@ -12,7 +12,7 @@ class Ping(commands.Cog):
     def cog_unload(self):
         self.updateServerStatus.cancel()
 
-    @tasks.loop(minutes=2)
+    @tasks.loop(minutes=10)
     async def updateServerStatus(self):
         if (ping(server_address)):
             await self.bot.change_presence(activity=discord.Game("Minecraft szerver fut"))

5d4e5d1e690b1ee4ea83285fd4ceb8695dca627e
levcs730 <levcs730@gmail.com>
2025-06-02 19:35:51 +0200

    masszív cog update és mostmár viszonylag clean a kód

---
+++ cogs/balint_reacting.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import constants
+
+class BalintReacting(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author != constants.REACTION_USER_ID:
+            return
+
+        for emoji in message.guild.emojis:
+            if emoji.id == constants.REACTION_EMOJI_ID:
+                await message.add_reaction(emoji)
---
+++ cogs/nemertem.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+import constants
+import re
+
+class Nemertem(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.id != constants.TARGET_USER_ID:
+            return
+        
+        if re.search("[ns]em* *[eé]rtem", message.content.lower()):
+            await message.reply("Nem meglepő")

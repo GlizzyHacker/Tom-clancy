0c77cd6b6b9f6d59845fedf861f8e2d3907d2a05
GlizzyHacker <saxattila@gmail.com>
2026-06-10 18:56:03 +0200

    switched to webhooks

--- cogs/encrypted_chat.py
+++ cogs/encrypted_chat.py
@@ -1,30 +1,39 @@
+import asyncio
+
 from discord.ext import commands
 import discord
 import constants
 import encrypt
 
-async def encrypt_command_callback(interaction, message: str):
+async def encrypt_command_callback(interaction: discord.Interaction, message: str):
     if interaction.user.id == constants.TARGET_USER_ID:
         await interaction.response.send_message("Ezt csak RNcS tagok használhatják.")
         return
-      
+    
+    await interaction.response.send_message("Yes king", ephemeral=True)
+
     words = message.split()
     encrypted_words = []
     for word in words:
         encrypted_words.append(encrypt.encrypt(word))
-    await interaction.response.send_message(interaction.user.mention + " ezt üzeni: " + " ".join(encrypted_words))
+
+    webhook = await interaction.channel.create_webhook(name=interaction.user.display_name, avatar=await interaction.user.display_avatar.read())
+    message = await webhook.send(
+        " ".join(encrypted_words),
+        wait=True  # For some reason if this line is not here message variable is NoneType, so I can't delete it
+    )
+    await webhook.delete()
         
 async def decrypt_command_callback(interaction: discord.Interaction, message: discord.Message):
     if interaction.user.id == constants.TARGET_USER_ID:
         await interaction.response.send_message("Ezt csak RNcS tagok használhatják.")
         return
     await interaction.response.defer(ephemeral=True)
-    if "ezt üzeni:" not in  message.content:
+    if message.webhook_id == None:
         await interaction.followup.send("Ezt nem így kell használni dummy", ephemeral=True)
         return
     
     words = message.content.split()
-    words = words[words.index("üzeni:")+1:]
     decrypted_words = []
     for word in words:
         decrypted_words.append(encrypt.decrypt(word))
